915a0f8656a27ec9f4afff93f75cfe330629b333
Mandeep Kaur <mandeep.kaur.fr@gmail.com>
2019-05-08 22:13:03 -0700

    Connnected with database Sql server

--- NewsLetterMVC/Controllers/HomeController.cs
+++ NewsLetterMVC/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -8,9 +10,42 @@ namespace NewsLetterMVC.Controllers
 {
     public class HomeController : Controller
     {
+        
         public ActionResult Index()
         {
             return View();
+        }
+        [HttpPost]
+        public ActionResult SignUp(string firstName,string lastName,string emailAddress)
+        {
+            if (string.IsNullOrEmpty(firstName) || string.IsNullOrEmpty(lastName) || string.IsNullOrEmpty(emailAddress))
+            {
+                return View("~/Views/Shared/Error.cshtml");
+            }
+            else
+            {
+                string _connectionString = @"Data Source=DESKTOP-LDP0GC0\SQLEXPRESS;
+                                            Initial Catalog=NewsLetter;Integrated Security=True;Connect Timeout=30;
+                                            Encrypt=False;TrustServerCertificate=False;
+                                            ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+                string queryString = @"Insert into SignUps (FirstName,LastName,EmailAddress)values
+                                    (@FirstName,@LastName,@EmailAddress)";
+                using(SqlConnection connection=new SqlConnection(_connectionString))
+                {
+                    SqlCommand command = new SqlCommand(queryString, connection);
+                    command.Parameters.Add("@FirstName", SqlDbType.VarChar);
+                    command.Parameters.Add("@LastName", SqlDbType.VarChar);
+                    command.Parameters.Add("@EmailAddress", SqlDbType.VarChar);
+                    command.Parameters["@FirstName"].Value = firstName;
+                    command.Parameters["@LastName"].Value = lastName;
+                    command.Parameters["@EmailAddress"].Value = emailAddress;
+
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                    connection.Close();
+                }
+                return View("Success");
+            }
         }
 
         public ActionResult About()
