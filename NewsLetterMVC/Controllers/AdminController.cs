2ef433e4ad5b4d5e9002393821980f1af0ee8c7c
Mandeep Kaur <mandeep.kaur.fr@gmail.com>
2019-05-09 16:15:23 -0700

    Added new project

--- NewsLetterMVC/Controllers/AdminController.cs
+++ NewsLetterMVC/Controllers/AdminController.cs
@@ -1,6 +1,8 @@
 ﻿using NewsLetterMVC.Models;
 using NewsLetterMVC.ViewModels;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Web.Mvc;
 
 namespace NewsLetterMVC.Controllers
@@ -12,19 +14,35 @@ namespace NewsLetterMVC.Controllers
         {
             using (NewsLetterEntities1 db = new NewsLetterEntities1())
             {
-                var signups = db.SignUps;
+
+                //var signups = db.SignUps.Where(x=> x.Removed==null).ToList();
+                var signups = (from c in db.SignUps
+                               where c.Removed == null
+                               select c).ToList();
                 var signupvm = new List<SignUpVm>();
                 foreach (var signup in signups)
                 {
                     var signupVm = new SignUpVm();
+                    signupVm.Id = signup.Id;
                     signupVm.FirstName = signup.FirstName;
                     signupVm.LastName = signup.LastName;
                     signupVm.EmailAddress = signup.EmailAddress;
                 }
+
                 return View(signupvm);
 
             }
            
+        }
+        public ActionResult Unsubcribe(int Id)
+        {
+            using(NewsLetterEntities1 db=new NewsLetterEntities1())
+            {
+                var signup = db.SignUps.Find(Id);
+                signup.Removed = DateTime.Now;
+                db.SaveChanges();
+            }
+            return RedirectToAction("Index");
         }
     }
 }
