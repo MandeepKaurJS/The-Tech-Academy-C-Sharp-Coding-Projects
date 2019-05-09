31c659d1e35c679c1be92f6626df1095c084707b
Mandeep Kaur <mandeep.kaur.fr@gmail.com>
2019-05-09 14:15:32 -0700

    update

--- NewsLetterMVC/Controllers/HomeController.cs
+++ NewsLetterMVC/Controllers/HomeController.cs
@@ -1,12 +1,4 @@
-﻿using NewsLetterMVC.Models;
-using NewsLetterMVC.ViewModels;
-using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Data.SqlClient;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace NewsLetterMVC.Controllers
 {
