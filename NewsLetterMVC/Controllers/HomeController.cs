df204ffb904ad67ff2ce8b34b6103f8943068b54
Mandeep Kaur <mandeep.kaur.fr@gmail.com>
2019-05-08 23:24:18 -0700

    update

--- NewsLetterMVC/Controllers/HomeController.cs
+++ NewsLetterMVC/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NewsLetterMVC.Models;
+using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
@@ -10,7 +11,12 @@ namespace NewsLetterMVC.Controllers
 {
     public class HomeController : Controller
     {
-        
+        private readonly string _connectionString = @"Data Source=DESKTOP-LDP0GC0\SQLEXPRESS;
+                                            Initial Catalog=NewsLetter;Integrated Security=True;Connect Timeout=30;
+                                            Encrypt=False;TrustServerCertificate=False;
+                                            ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+
         public ActionResult Index()
         {
             return View();
@@ -24,10 +30,6 @@ namespace NewsLetterMVC.Controllers
             }
             else
             {
-                string _connectionString = @"Data Source=DESKTOP-LDP0GC0\SQLEXPRESS;
-                                            Initial Catalog=NewsLetter;Integrated Security=True;Connect Timeout=30;
-                                            Encrypt=False;TrustServerCertificate=False;
-                                            ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
                 string queryString = @"Insert into SignUps (FirstName,LastName,EmailAddress)values
                                     (@FirstName,@LastName,@EmailAddress)";
                 using(SqlConnection connection=new SqlConnection(_connectionString))
@@ -50,7 +52,24 @@ namespace NewsLetterMVC.Controllers
 
         public ActionResult Admin()
         {
-            return View();
+            string querystrin = "Select Id,FirstName,LastName,EmailAddress from SignUps";
+            List<NewsLetterSignUp> signups = new List<NewsLetterSignUp>();
+            using(SqlConnection connection=new SqlConnection(_connectionString))
+            {
+                SqlCommand command = new SqlCommand(querystrin, connection);
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    var signup = new NewsLetterSignUp();
+                    signup.id = Convert.ToInt32(reader["Id"]);
+                    signup.FirstName = reader["FirstName"].ToString();
+                    signup.LastName = reader["LastName"].ToString();
+                    signup.EmailAddress = reader["EmailAddress"].ToString();
+                    signups.Add(signup);
+                }
+            }
+            return View(signups);
         }
     }
 }
