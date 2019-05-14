ac8ff86af00c63897f6284961a09bc11ce5a170e
Mandeep Kaur <mandeep.kaur.fr@gmail.com>
2019-05-14 15:08:14 -0700

    update

---
+++ ConsoleAppForDrill/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Office.Interop.Excel;
+//using _Excel = Microsoft.Office.interop.Excel;
+namespace ConsoleAppForDrill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         
+         string fileName, Sampletext;
+         Console.Write("Enter File Name :");
+         fileName = Console.ReadLine();
+         Console.Write("Enter text :");
+         Sampletext = Console.ReadLine();
+            //Create excel app object
+         Microsoft.Office.Interop.Excel.Application xlSamp = new Microsoft.Office.Interop.Excel.Application();
+            if (xlSamp == null)
+            {
+                Console.WriteLine("Excel is not Insatalled");
+                Console.ReadKey();
+                return;
+            }
+            //Create a new excel book and sheet
+            Microsoft.Office.Interop.Excel.Workbook xlWorkBook;
+            Microsoft.Office.Interop.Excel.Worksheet xlWorkSheet;
+            object misValue = System.Reflection.Missing.Value;
+            //Then add a sample text into first cell
+            xlWorkBook = xlSamp.Workbooks.Add(misValue);
+            xlWorkSheet = (Microsoft.Office.Interop.Excel.Worksheet)xlWorkBook.Worksheets.get_Item(1);
+            xlWorkSheet.Cells[1, 1] = Sampletext;
+            string location = @"C:\Users\VrinMan Dulay\Documents\" + fileName + ".xls";
+            xlWorkBook.SaveAs(location, Microsoft.Office.Interop.Excel.XlFileFormat.xlWorkbookNormal, misValue, misValue, misValue, misValue, Microsoft.Office.Interop.Excel.XlSaveAsAccessMode.xlExclusive, misValue, misValue, misValue, misValue, misValue);
+            xlWorkBook.Close(true, misValue, misValue);
+            xlSamp.Quit();
+            try
+            {
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(xlSamp);
+                xlSamp = null;
+            }
+            catch (Exception ex)
+            {
+                xlSamp = null;
+                Console.Write("Error " + ex.ToString());
+            }
+            finally
+            {
+                GC.Collect();
+            }
+        }
+    }
+
+}
+
