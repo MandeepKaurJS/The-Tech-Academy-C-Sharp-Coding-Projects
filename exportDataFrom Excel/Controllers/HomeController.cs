3505698dcd5902c9dfe82dc9658abea8916bc386
Mandeep Kaur <mandeep.kaur.fr@gmail.com>
2019-05-14 15:39:34 -0700

    update

---
+++ exportDataFrom Excel/Controllers/HomeController.cs
@@ -0,0 +1,74 @@
+﻿using exportDataFrom_Excel.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.OleDb;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace exportDataFrom_Excel.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(ImportExcel importExcel)
+        {
+            if (ModelState.IsValid)
+            {
+                string path = Server.MapPath("~/Content/Upload/" + importExcel.file.FileName);
+                importExcel.file.SaveAs(path);
+
+                string excelConnectionString = @"Provider='Microsoft.ACE.OLEDB.12.0';Data Source='" + path + "';Extended Properties='Excel 12.0 Xml;IMEX=1'";
+                OleDbConnection excelConnection = new OleDbConnection(excelConnectionString);
+
+                //Sheet Name
+                excelConnection.Open();
+                string tableName = excelConnection.GetSchema("Tables").Rows[0]["TABLE_NAME"].ToString();
+                excelConnection.Close();
+                //End
+
+                OleDbCommand cmd = new OleDbCommand("Select * from [" + tableName + "]", excelConnection);
+
+                excelConnection.Open();
+
+                OleDbDataReader dReader;
+                dReader = cmd.ExecuteReader();
+                SqlBulkCopy sqlBulk = new SqlBulkCopy(ConfigurationManager.ConnectionStrings["CS"].ConnectionString);
+
+                //Give your Destination table name
+                sqlBulk.DestinationTableName = "Student";
+
+                //Mappings
+                sqlBulk.ColumnMappings.Add("FirstName", "FirstName");
+                sqlBulk.ColumnMappings.Add("LastName", "LastName");
+                
+
+                sqlBulk.WriteToServer(dReader);
+                excelConnection.Close();
+
+                ViewBag.Result = "Successfully Imported";
+            }
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ exportDataFrom Excel/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="exportDataFrom_Excel.MvcApplication" Language="C#" %>
