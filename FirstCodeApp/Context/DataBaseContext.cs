721dd4649ad49bf2890730f71d25c6e9a158f271
Mandeep Kaur <mandeep.kaur.fr@gmail.com>
2019-05-14 13:01:15 -0700

    Added project

---
+++ FirstCodeApp/Context/DataBaseContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace FirstCodeApp.Context
+{
+    public class DataBaseContext:DbContext
+    {
+        public DatabaseContext() : base("StudentDatabase")
+        { }
+        public DbSet<Student> Students { get; set; }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
---
+++ FirstCodeApp/Global.asax.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace FirstCodeApp
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            Database.SetInitializer(new DatabaseInitializer()); undleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
