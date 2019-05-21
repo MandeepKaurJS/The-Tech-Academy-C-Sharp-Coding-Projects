ee465f386c7925d02a7e8c2d9a9fd91980b79b4a
Mandeep Kaur <mandeep.kaur.fr@gmail.com>
2019-05-20 22:33:06 -0700

    update

--- ConsoleAppForDrill/Program.cs
+++ ConsoleAppForDrill/Program.cs
@@ -1,75 +1,73 @@
-﻿
-using Syncfusion.XlsIO;
+﻿using System;
 using System.IO;
-using System.Reflection;
-//using _Excel = Microsoft.Office.interop.Excel;
+using Microsoft.Office.Interop.Excel;
 namespace ConsoleAppForDrill
 {
     class Program
     {
         static void Main(string[] args)
         {
-            //Create an instance of ExcelEngine
-            using (ExcelEngine excelEngine = new ExcelEngine())
-            {
-                IApplication application = excelEngine.Excel;
-                //Set the default application version as Excel 2016
-                excelEngine.Excel.DefaultVersion = ExcelVersion.Excel2016;
+            ////Create an instance of ExcelEngine
+            //using (ExcelEngine excelEngine = new ExcelEngine())
+            //{
+            //    IApplication application = excelEngine.Excel;
+            //    //Set the default application version as Excel 2016
+            //    excelEngine.Excel.DefaultVersion = ExcelVersion.Excel2016;
 
-                //Open existing workbook with data entered
-                Assembly assembly = typeof(Program).GetTypeInfo().Assembly;
-                Stream fileStream = assembly.GetManifestResourceStream("name.Sample.xls");
-                IWorkbook workbook = application.Workbooks.Open(fileStream);
+            //    //Open existing workbook with data entered
+            //    Assembly assembly = typeof(Program).GetTypeInfo().Assembly;
+            //    Stream fileStream = assembly.GetManifestResourceStream("name.xlsx");
+            //    IWorkbook workbook = application.Workbooks.Open(fileStream);
 
-                //Access first worksheet from the workbook instance
-                IWorksheet worksheet = workbook.Worksheets[0];
+            //    //Access first worksheet from the workbook instance
+            //    IWorksheet worksheet = workbook.Worksheets[0];
 
-                //Insert sample text into cell “A1”
-                worksheet.Range["A1"].Text = "Syncfusion Essential XlsIO";
+            //    //Insert sample text into cell “A1”
+            //    worksheet.Range["A1"].Text = "Syncfusion Essential XlsIO";
 
-                //Save the workbook to disk in xlsx format
-                workbook.SaveAs("Output.xlsx");
+            //    //Save the workbook to disk in xlsx format
+            //    workbook.SaveAs("Output.xlsx");
+            //}
+
+            string filename, sampletext;
+            Console.Write("enter file name :");
+            filename = Console.ReadLine();
+            Console.Write("enter text :");
+            sampletext =Console.ReadLine();
+            //create excel app object
+            Microsoft.Office.Interop.Excel.Application xlsamp = new Microsoft.Office.Interop.Excel.Application();
+            if(xlsamp == null)
+               {
+                Console.WriteLine("excel is not insatalled");
+                Console.ReadKey();
+                return;
             }
-
-            //string fileName, Sampletext;
-            //Console.Write("Enter File Name :");
-            //fileName = Console.ReadLine();
-            //Console.Write("Enter text :");
-            //Sampletext = Console.ReadLine();
-            //   //Create excel app object
-            //Microsoft.Office.Interop.Excel.Application xlSamp = new Microsoft.Office.Interop.Excel.Application();
-            //   if (xlSamp == null)
-            //   {
-            //       Console.WriteLine("Excel is not Insatalled");
-            //       Console.ReadKey();
-            //       return;
-            //   }
-            //   //Create a new excel book and sheet
-            //   Microsoft.Office.Interop.Excel.Workbook xlWorkBook;
-            //   Microsoft.Office.Interop.Excel.Worksheet xlWorkSheet;
-            //   object misValue = System.Reflection.Missing.Value;
-            //   //Then add a sample text into first cell
-            //   xlWorkBook = xlSamp.Workbooks.Add(misValue);
-            //   xlWorkSheet = (Microsoft.Office.Interop.Excel.Worksheet)xlWorkBook.Worksheets.get_Item(1);
-            //   xlWorkSheet.Cells[1, 1] = Sampletext;
-            //   string location = @"C:\Users\VrinMan Dulay\Documents\" + fileName + ".xls";
-            //   xlWorkBook.SaveAs(location, Microsoft.Office.Interop.Excel.XlFileFormat.xlWorkbookNormal, misValue, misValue, misValue, misValue, Microsoft.Office.Interop.Excel.XlSaveAsAccessMode.xlExclusive, misValue, misValue, misValue, misValue, misValue);
-            //   xlWorkBook.Close(true, misValue, misValue);
-            //   xlSamp.Quit();
-            //   try
-            //   {
-            //       System.Runtime.InteropServices.Marshal.ReleaseComObject(xlSamp);
-            //       xlSamp = null;
-            //   }
-            //   catch (Exception ex)
-            //   {
-            //       xlSamp = null;
-            //       Console.Write("Error " + ex.ToString());
-            //   }
-            //   finally
-            //   {
-            //       GC.Collect();
-            //   }
+            //create a new excel book and sheet
+            Microsoft.Office.Interop.Excel.Workbook xlworkbook;
+            Microsoft.Office.Interop.Excel.Worksheet xlworksheet;
+            object misvalue = System.Reflection.Missing.Value;
+            //then add a sample text into first cell
+            xlworkbook = xlsamp.Workbooks.Add(misvalue);
+            xlworksheet = (Microsoft.Office.Interop.Excel.Worksheet)xlworkbook.Worksheets.get_Item(1);
+            xlworksheet.Cells[1, 1] = sampletext;
+            string location = @"c:\users\vrinman dulay\documents\" + filename + ".xls";
+            xlworkbook.SaveAs(location, Microsoft.Office.Interop.Excel.XlFileFormat.xlWorkbookNormal, misvalue, misvalue, misvalue, misvalue, Microsoft.Office.Interop.Excel.XlSaveAsAccessMode.xlExclusive, misvalue, misvalue, misvalue, misvalue, misvalue);
+            xlworkbook.Close(true, misvalue, misvalue);
+            xlsamp.Quit();
+            try
+               {
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(xlsamp);
+                xlsamp = null;
+            }
+            catch(Exception ex)
+               {
+                xlsamp = null;
+                Console.Write("error " + ex.ToString());
+            }
+            finally
+               {
+                GC.Collect();
+            }
         }
     }
 
---
+++ ExportExcelFileInASP.NET/Controllers/ProductController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Excel = Microsoft.Office.Interop.Excel;
+
+using ExportExcelFileInASP.NET.Models;
+
+namespace ExportExcelFileInASP.NET.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Import(HttpPostedFileBase excelfile)
+        {
+            if (excelfile==null || excelfile.ContentLength == 0)
+            {
+                ViewBag.Error = "Please select Excel file<br>";
+                return View("Index");
+            }
+            else
+            {
+                if (excelfile.FileName.EndsWith("xls") || excelfile.FileName.EndsWith("xlsx"))
+                {
+                    string path = Server.MapPath("~/Content/" + excelfile.FileName);
+                    if (System.IO.File.Exists(path))
+                        System.IO.File.Delete(path);
+                    excelfile.SaveAs(path);
+                    //Read data from excel file
+                    Excel.Application application = new Excel.Application();
+                    Excel.Workbook workbook = application.Workbooks.Open(path);
+                    Excel.Worksheet worksheet = workbook.ActiveSheet;
+                    Excel.Range rang = worksheet.UsedRange;
+                    List<Product> listproduct = new List<Product>();
+                    for (int row = 3; row < rang.Rows.Count; row++)
+                    {
+                        Product p = new Product();
+                        p.id = ((Excel.Range)rang.Cells[row, 1]).Text;
+                        p.FirstName = ((Excel.Range)rang.Cells[row, 2]).Text;
+                        p.LastName = ((Excel.Range)rang.Cells[row, 3]).Text;
+                        listproduct.Add(p);
+                    }
+                    ViewBag.ListProducts = listproduct;
+                    return View("Success");
+                }
+                else
+                {
+                    ViewBag.Error = "File type is incorrect<br>";
+                    return View("Index");
+                }
+                return View();
+            }
+            
+        }
+    }
+}
