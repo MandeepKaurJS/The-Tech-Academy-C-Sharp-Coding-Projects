fc824df36370c6e56d71899f1b3a8261adf59810
Mandeep Kaur <mandeep.kaur.fr@gmail.com>
2019-05-21 15:48:09 -0700

    update

---
+++ ExcelToSqlDatabase/ExcelData.aspx
@@ -0,0 +1,55 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ExcelData.aspx.cs" Inherits="ExcelToSqlDatabase.ExcelData" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            width: 213px;
+        }
+        .auto-style2 {
+            width: 213px;
+            height: 65px;
+        }
+        .auto-style3 {
+            height: 65px;
+            width: 345px;
+        }
+        .auto-style4 {
+            width: 345px;
+        }
+        .auto-style5 {
+            background-color: #FF9900;
+        }
+        .auto-style6 {
+            background-color: #CCFFFF;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <p>
+        Import Excel Data to Sql Database</p>
+    <p>
+        Upload Excel file and Read Data Then Save to SQL Server in ASP.NET</p>
+        <table class="auto-style5" style="width: 46%;">
+            <tr>
+                <td class="auto-style2">Upload Excel File</td>
+                <td class="auto-style3">
+                    <asp:FileUpload ID="FileUpload1" runat="server" CssClass="auto-style6" />
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style1">&nbsp;</td>
+                <td class="auto-style4">
+                    <asp:Button ID="Button1" runat="server" BorderColor="#CC6600" ForeColor="#990000" Text="Upload And Save To Sql Server" OnClick="Button1_Click1" style="height: 29px" />
+                </td>
+            </tr>
+        </table>
+        <asp:Label ID="Label1" runat="server" Font-Bold="True" Font-Size="Large"></asp:Label>
+    </form>
+    
+</body>
+</html>
---
+++ ExcelToSqlDatabase/ExcelData.aspx.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.IO;
+using System.Data.OleDb;
+using System.Data.SqlClient;
+
+namespace ExcelToSqlDatabase
+{
+    public partial class ExcelData : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void Button1_Click1(object sender, EventArgs e)
+        {
+            int rollno;
+            String sname;
+            String fname;
+            String mname;
+            string path = Path.GetFileName(FileUpload1.FileName);
+            //path = path.Replace("","");
+            FileUpload1.SaveAs(Server.MapPath("~/ExcelFile/") + path);
+            String ExcelPath = Server.MapPath("~/ExcelFile/") + path;
+            OleDbConnection mycon=new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + ExcelPath +"; Extended Properties=Excel 8.0; Persist Security Info=false");
+            mycon.Open();
+            OleDbCommand cmd = new OleDbCommand("select * from [Sheet1$]", mycon);
+            OleDbDataReader dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                if (dr[0].ToString() != "")
+                {
+                    rollno = Convert.ToInt32(dr[0].ToString());
+                    sname = dr[1].ToString();
+                    fname = dr[2].ToString();
+                    mname = dr[3].ToString();
+                    savedata(rollno, sname, fname, mname);
+                }
+                else
+                {
+                    break;
+                }
+             
+            }
+            Label1.Text = "Data Has Been Saved Successfully";
+            mycon.Close();
+        }
+        private void savedata(int rollno1,String sname1,String fname1,String mname1)
+        {
+            String query="insert into StudentsRecord(rollno,sname,fathername,mothername) values("+rollno1+",'"+sname1+"','"+fname1+"','"+mname1+"')";
+            String mycon = "Data Source=DESKTOP-LDP0GC0\\SQLEXPRESS;Initial Catalog=StudentDetails;Integrated Security=true";
+            SqlConnection con = new SqlConnection(mycon);
+            con.Open();
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = query;
+            cmd.Connection = con;
+            cmd.ExecuteNonQuery();
+        }
+        //private void Updatedata(int rollno1, String sname1, String fname1, String mname1)
+        //{
+        //    String query = "update StudentRecord set sname='" + sname1 + "',+fathername='" + fname1 + "',mothername='" + mname1 + "' where rollno=" + rollno1;
+        //    String mycon = "Data Source=DESKTOP-LDP0GC0\\SQLEXPRESS;Initial Catalog=StudentDetails;Integrated Security=true";
+        //    SqlConnection con = new SqlConnection(mycon);
+        //    con.Open();
+        //    SqlCommand cmd = new SqlCommand();
+        //    cmd.CommandText = query;
+        //    cmd.Connection = con;
+        //    cmd.ExecuteNonQuery();
+        //}
+
+      
+    }
+}
