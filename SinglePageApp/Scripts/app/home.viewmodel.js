f3f68dfcc481333b06d5f1eb952f1ad08b9c86ca
Mandeep Kaur <mandeep.kaur.fr@gmail.com>
2019-05-20 19:06:01 -0700

    update

--- SinglePageApp/Scripts/app/home.viewmodel.js
+++ SinglePageApp/Scripts/app/home.viewmodel.js
@@ -29,3 +29,41 @@ app.addViewModel({
     bindingMemberName: "home",
     factory: HomeViewModel
 });
+    var candidateModel = {
+        FirstName: ko.observable(''),
+    LastName: ko.observable(''),
+    Email: ko.observable(''),
+    Experience: ko.observable(0),
+    Technologies: ko.observableArray([]),
+    TechnologyToAdd: ko.observable('')
+};
+
+
+candidateModel.Name = ko.dependentObservable(function () {
+    return candidateModel.FirstName() +
+        " " + candidateModel.LastName();
+});
+
+
+
+candidateModel.addTechnology = function () {
+    if (candidateModel.TechnologyToAdd() !== '') {
+        candidateModel.Technologies.push(candidateModel.TechnologyToAdd());
+        candidateModel.TechnologyToAdd('');
+    }
+};
+ko.applyBindings(candidateModel);
+
+
+candidateModel.addCandidate = function () {
+    $.ajax({
+        url: "/Home/Create/",
+        type: 'post',
+        data: ko.toJSON(this),
+        contentType: 'application/json',
+        success: function (result) {
+            alert(result);
+        }
+    });
+};
+
